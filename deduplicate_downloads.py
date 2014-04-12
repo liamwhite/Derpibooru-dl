d8971e9642e802a4d50d24c734004985d042c8c9
James <J1459.j1459@gmail.com>
2014-04-12 16:14:26 +0800

    deduplicator has basic functionality now

--- deduplicate_downloads.py
+++ deduplicate_downloads.py
@@ -12,8 +12,10 @@
 
 import derpibooru_dl
 import logging
-
-
+import os
+import glob
+import re
+import shutil
 
 class settings_handler:
     def __init__(self):
@@ -21,8 +23,8 @@ class settings_handler:
         return
 
     def set_defaults(self):
-        self.downloads_folder = ""# Folder to process
-        self.output_folder = "combined_downloads"# Folder to output to
+        self.downloads_folder = "G:\derpibooru_dl\download"# Folder to process
+        self.output_folder = os.path.join(self.downloads_folder,"combined_downloads")# Folder to output to
         self.use_tag_list = True# Use tag list instead of processing everything
         self.move = False# Move files instead of copying them
         self.reverse = False
@@ -33,20 +35,20 @@ class settings_handler:
         return
 
 
-def process_submission_data_tuple(settings,submission_data_tuples):
+def process_submission_data_tuple(settings,submission_data_tuple):
     # Build expected paths
-    input_dir, match_filename = os.path.split(glob_match)
-    expected_json_input_filename = submission_id+".json"
-    expected_json_input_location = submission_data_tuples[1]
-    json_output_folder = os.path.join(output_folder, "json")
-    json_output_filename = submission_data_tuples[2]+".json"
+    input_filepath, json_filepath, submission_id = submission_data_tuple
+    input_dir, match_filename = os.path.split(input_filepath)
+    json_output_filename = os.path.split(json_filepath)[1]
+    json_output_folder = os.path.join(settings.output_folder, "json")
+
     json_output_path = os.path.join(json_output_folder, json_output_filename)
     submission_output_path = os.path.join(settings.output_folder,match_filename)
     # Ensure output path exists
     if not os.path.exists(json_output_folder):
         os.makedirs(json_output_folder)
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    if not os.path.exists(settings.output_folder):
+        os.makedirs(settings.output_folder)
     # Check that both files exist in the input location and skip if either is missing
     # Ensure output location exists
     # Depending on mode, wither copy or move files to output location
@@ -58,21 +60,23 @@ def process_submission_data_tuple(settings,submission_data_tuples):
         logging.info("Copying files for submission: "+submission_id+" from "+input_dir+" to "+settings.output_folder)
         try:
             # Copy submission file
-            shutil.copy2(glob_match, submission_output_path)
+            shutil.copy2(input_filepath, submission_output_path)
             # Copy JSON
-            shutil.copy2(expected_json_input_location, json_output_path)
+            shutil.copy2(json_filepath, json_output_path)
             return True
         except IOError, err:
             logging.error("Error copying files!")
             logging.exception(err)
             return False
 
-
-def join_submission_data_lists(settings,image_files_list,json_files_list):
-    # Data tuple formats:
-    # image_tuple = (image_file_path, image_id)
-    # json_tuple = (json_file_path, json_id)
-    # joined_file_tuple = (image_file_path, json_file_path, image_id)
+def generate_image_tuples(settings,input_folder_path):
+    """Generate a list of tuples of image file paths and their id numbers.
+    e.g. ("download\\derpi_12345.jpg", "12345")"""
+    # generate glob string for images
+    image_glob_string = os.path.join(input_folder_path, settings.filename_prefix+"*")
+    # list image files in folder
+    image_files_list = glob.glob(image_glob_string)
+    print "image_files_list", image_files_list
     # Extract submission ids from filenames
     image_tuples = []
     for image_file_path in image_files_list:
@@ -83,16 +87,33 @@ def join_submission_data_lists(settings,image_files_list,json_files_list):
             image_tuple = (image_file_path, image_id)
             image_tuples.append(image_tuple)
     print "image_tuples", image_tuples
-    # Extract JSON ids
+    return image_tuples
+
+def generate_json_tuples(settings,input_folder_path):
+    """Generate a list of tuples of json file paths and their id numbers.
+    e.g. ("download\\json\\12345.json", "12345")"""
+    # Generate JSON glob string
+    json_glob_string = os.path.join(input_folder_path,"json","*.json")
+    # list json files
+    json_files_list = glob.glob(json_glob_string)
+    print "json_files_list", json_files_list
+     # Extract JSON ids
     json_tuples = []
     for json_file_path in json_files_list:
         json_id_regex = "(\d+)\.json"
         json_id_search = re.search(json_id_regex,json_file_path)
         if json_id_search:
-            json_id = image_id_search.group(1)
+            json_id = json_id_search.group(1)
             json_tuple = (json_file_path, json_id)
             json_tuples.append(json_tuple)
     print "json_tuples", json_tuples
+    return json_tuples
+
+def join_submission_data_lists(settings,image_tuples,json_tuples):
+    # Data tuple formats:
+    # image_tuple = (image_file_path, image_id)
+    # json_tuple = (json_file_path, json_id)
+    # joined_file_tuple = (image_file_path, json_file_path, image_id)
     # Sort and join tuples
     joined_files_tuples = []
     for image_tuple in image_tuples:
@@ -110,17 +131,11 @@ def join_submission_data_lists(settings,image_files_list,json_files_list):
 
 
 
-def generate_submission_data_pairs(settings,input_folder_path):
-    # generate glob string for images
-    image_glob_string = input_folder_path+settings.filename_prefix+"*"
-    # list image files in folder
-    image_glob_matches = glob.glob(glob_string)
-    # Generate JSON glob string
-    json_glob_string = input_folder_path+"*.json"
-    # list json files
-    json_glob_matches = glob.glob(glob_string)
+def generate_submission_data_tuples(settings,input_folder_path):
+    image_tuples = generate_image_tuples(settings,input_folder_path)
+    json_tuples = generate_json_tuples(settings,input_folder_path)
     # join lists into pairs of filepaths for each submission and its associated JSON
-    submission_data_tuples = join_submission_data_lists(settings,image_files_list,json_files_list)
+    submission_data_tuples = join_submission_data_lists(settings,image_tuples,json_tuples)
     return submission_data_tuples
 
 
@@ -131,7 +146,7 @@ def process_folder(settings,folder_name):
         logging.error("specified folder does not exist, cannot process it."+input_folder_path)
         return
     # Buld pairs of submission + metadata files to process
-    submission_data_tuples = generate_submission_data_pairs(settings,input_folder_path)
+    submission_data_tuples = generate_submission_data_tuples(settings,input_folder_path)
     # Process each pair
     for submission_data_tuple in submission_data_tuples:
         process_submission_data_tuple(settings, submission_data_tuple)
