41b5bfa7f018627d95c891f93148393feb85665d
James <J1459.j1459@gmail.com>
2014-07-03 11:18:32 +0800

    WIP for executable

---
+++ make_exe.bat
@@ -0,0 +1,2 @@
+REM This is just to build a .exe with py2exe
+python make_exe.py py2exe
---
+++ make_exe.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      new
+#
+# Created:     15/04/2014
+# Copyright:   (c) new 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+
+
+
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['derpibooru_dl.py'])
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
