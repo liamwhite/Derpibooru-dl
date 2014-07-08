897fa76c8e6cbe2e1df6499fb0c728875f8e2d59
James <J1459.j1459@gmail.com>
2014-07-08 22:22:38 +0800

    Starting work on script to copy local files to folders based on tags

---
+++ split_to_tag_folders.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        copy_to_tag_folders
+# Purpose:
+#
+# Author:      new
+#
+# Created:     08/07/2014
+# Copyright:   (c) new 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
